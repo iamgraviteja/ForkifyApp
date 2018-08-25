45353688c77f7b3ce1df297c01306f4883df2caa
ravi2808 <ravi.cool2808@gmail.com>
2018-08-25 20:29:57 +0530

    Added files related to forkify

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+import num from './test';
+
+const num2 = 54;
+console.log(`I'm from index.js reading ${num} from imported file. The const number is ${num2}`);
---
+++ src/js/test.js
@@ -0,0 +1,3 @@
+console.log('From test.js');
+
+export default 1600;
